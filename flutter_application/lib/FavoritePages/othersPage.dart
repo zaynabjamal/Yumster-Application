c3caca2461c872c1af5fbdee85f6b14d696eb9d2
zaynabjamal <zaynab.software.21@gmail.com>
2024-02-27 01:03:35 +0300

    alittleUpdate

--- flutter_application/lib/FavoritePages/othersPage.dart
+++ flutter_application/lib/FavoritePages/othersPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_application/FavoritePages/savePage.dart';
 import 'package:flutter_application/data/food_type_data.dart';
 import 'package:flutter_application/model/food_type.dart';
@@ -11,14 +12,14 @@ class Others extends StatefulWidget {
 
   @override
   State<Others> createState() => _OthersState();
-} 
+}
 
 class _OthersState extends State<Others> {
-  
-
   @override
   Widget build(BuildContext context) {
-    final provider  = Provider.of<bookmarkProvider>(context, listen:false);//contexte buildContext
+    bool _isBookmark = false;
+    final provider = Provider.of<bookmarkProvider>(context,
+        listen: false); //contexte buildContext
     return Scaffold(
       backgroundColor: const Color(0xffFCFCF8),
       appBar: AppBar(
@@ -34,28 +35,15 @@ class _OthersState extends State<Others> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Expanded(
-            child: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,mainAxisExtent: 220,), 
+              child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisExtent: 220,
+            ),
             itemCount: 4,
-            itemBuilder: (context,index){
-             return GestureDetector(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => SavedPage()));
-              },
-               child: Container(
-                  width: 100,
-                  height: 130,
-                  decoration: const BoxDecoration(
-                    color: Color(0xFFFFF4E5),
-                    borderRadius: BorderRadius.all(Radius.circular(4)),
-                  ),
-                  child: Stack(
-                    children: [
-                      Image.asset(foodTypeData[index].image,),
-                      Column(
-                        children: [
-                           Padding(
-                padding: const EdgeInsets.only(right: 95, top: 3),
-                child: Container(
+            itemBuilder: (_, index) => GridTile(
+              header: GridTileBar(
+                leading: Container(
                   width: 50,
                   height: 12,
                   decoration: const BoxDecoration(
@@ -81,49 +69,136 @@ class _OthersState extends State<Others> {
                     ),
                   ),
                 ),
-                           ),
-                           const Gap(85),
-                           Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Gap(5),
-                  Text(
-                   foodTypeData[index].title ,
-                    style: const TextStyle(
-                      color: Color(0xFF697C37),
-                      fontFamily: 'Rowdies',
-                      fontSize: 15,
-                      fontWeight: FontWeight.w300,
-                    ),
-                  ),
-                  const Gap(15),
-                  GestureDetector(
-                    onTap: (){
-                      setState(() {
-                        foodTypeData[index].bookmark = !foodTypeData[index].bookmark;
-                        provider.addItems(food_type);
-                        print("item added to provider!!");
-                      print("${ foodTypeData[index].id} this is id");
-                      });
-                    },
-                    child: Container(
-                      child: foodTypeData[index].bookmark
-                    ? Icon(Icons.bookmark)
-                    : Icon(Icons.bookmark_border),
-                    ),
-                  )
-                ],
-                           ),
-                        ],
+              ),
+              footer: GridTileBar(
+                backgroundColor: Color(0xFFFFF4E5),
+                leading: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      foodTypeData[index].title,
+                      style: const TextStyle(
+                        color: Color(0xFF697C37),
+                        fontFamily: 'Rowdies',
+                        fontSize: 15,
+                        fontWeight: FontWeight.w300,
                       ),
-                    ],
-                  ),
+                    ),
+                    GestureDetector(
+                     onTap: (){
+               setState(() {
+               foodTypeData[index].bookmark = !foodTypeData[index].bookmark;
+               provider.addItems(food_type);
+               print("item added to provider!!");
+             print("${ foodTypeData[index].id} this is id");
+               });
+             },
+                    child: Container(
+                     child: foodTypeData[index].bookmark
+             ? const Icon(Icons.bookmark , color: Color(0xFFFE9801),)
+             : const Icon(Icons.bookmark_border , color: Color(0xFFFE9801)),
+                    ),
+                   )
+                  ],
                 ),
-             );
-            },),
+              ),
+              child: Image.asset(
+                foodTypeData[index].image,
+                fit: BoxFit.cover,
+              ),
+            ),
+
+            // itemBuilder: (context,index)
+
+            // {
+            //  return GestureDetector(
+            //   onTap: (){
+            //     Navigator.push(context, MaterialPageRoute(builder: (context) => SavedPage()));
+            //   },
+
+            //    child: Container(
+            //       width: 154,
+            //       height: 130,
+            //       decoration: const BoxDecoration(
+            //         color: Color(0xFFFFF4E5),
+            //         borderRadius: BorderRadius.all(Radius.circular(4)),
+            //       ),
+            //       child: Stack(
+            //         children: [
+            //           Image.asset(foodTypeData[index].image,),
+            //           Column(
+            //             children: [
+            //                Padding(
+            //     padding: const EdgeInsets.only(right: 95, top: 3),
+            //     child: Container(
+            //       width: 50,
+            //       height: 12,
+            //       decoration: const BoxDecoration(
+            //         color: Color(0xFF697C37),
+            //         borderRadius: BorderRadius.all(Radius.circular(4)),
+            //       ),
+            //       child: Padding(
+            //         padding: const EdgeInsets.only(left: 6),
+            //         child: Row(
+            //           children: [
+            //             const Icon(
+            //               Icons.access_time,
+            //               color: Color(0xFFFFF4E5),
+            //               size: 9,
+            //             ),
+            //             const Gap(5),
+            //             Text(
+            //               '${foodTypeData[index].time}sec',
+            //               style: const TextStyle(
+            //                   fontSize: 8, color: Color(0xFFFFF4E5)),
+            //             ),
+            //           ],
+            //         ),
+            //       ),
+            //     ),
+            //                ),
+            //                const Gap(85),
+            //                Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     children: [
+            //       const Gap(5),
+            //       Text(
+            //        foodTypeData[index].title ,
+            //         style: const TextStyle(
+            //           color: Color(0xFF697C37),
+            //           fontFamily: 'Rowdies',
+            //           fontSize: 15,
+            //           fontWeight: FontWeight.w300,
+            //         ),
+            //       ),
+            //       const Gap(15),
+            //       GestureDetector(
+            //         onTap: (){
+            //   setState(() {
+            //   foodTypeData[index].bookmark = !foodTypeData[index].bookmark;
+            //   provider.addItems(food_type);
+            //   print("item added to provider!!");
+            // print("${ foodTypeData[index].id} this is id");
+            //   });
+            // },
+            //         child: Container(
+            //           child: foodTypeData[index].bookmark
+            // ? const Icon(Icons.bookmark)
+            // : const Icon(Icons.bookmark_border),
+            //         ),
+            //       )
+            //     ],
+            //                ),
+            //             ],
+            //           ),
+            //         ],
+            //       ),
+            //     ),
+            //  );
+            // },),
           )
-          //const Gap(32),
-          /*Row(
+              //const Gap(32),
+              /*Row(
             children: [
               const Gap(32),
               SvgPicture.asset("assets/ArrowBackVector.svg"),
@@ -138,13 +213,11 @@ class _OthersState extends State<Others> {
               )
             ],
           ),*/
-          //const Gap(42),
-          
-      
+              //const Gap(42),
+
+              )
         ],
-             
       ),
     );
   }
 }
-
--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application/Catagories/others_screen.dart';
+import 'package:flutter_application/FavoritePages/othersPage.dart';
 import 'package:flutter_application/provider/Bookmark.dart';
 import 'package:provider/provider.dart';
 
