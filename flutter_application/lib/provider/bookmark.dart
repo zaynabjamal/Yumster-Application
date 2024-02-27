c0aac76458f5b2a24f8e7b6956d4b4b6e2c2c129
bawar <bawarkhalid912@gmail.com>
2024-02-27 12:48:14 +0300

    android gradle typo error

--- flutter_application/lib/provider/bookmark.dart
+++ flutter_application/lib/provider/bookmark.dart
@@ -1,26 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/model/food_type.dart';
 
-class bookmarkProvider extends ChangeNotifier{
+class bookmarkProvider extends ChangeNotifier {
+  final List<FoodTypeModel> _widget = [];
+  List<FoodTypeModel> get Widget => _widget;
 
-final List<FoodTypeModel> _widget=[];
-List<FoodTypeModel> get Widget => _widget;
+  void addItems(FoodTypeModel items) {
+    _widget.add(items);
+    notifyListeners();
+  }
 
-void addItems(FoodTypeModel items){
-  _widget.add(items);
-  notifyListeners();
-}
-
-void removeItem(FoodTypeModel items){
-  _widget.remove(items);
-  notifyListeners();
-}
-
-
-
-
-
-
-
-
+  void removeItem(FoodTypeModel items) {
+    _widget.remove(items);
+    notifyListeners();
+  }
 }
