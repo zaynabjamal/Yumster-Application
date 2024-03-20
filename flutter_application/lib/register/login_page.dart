b8c25a5efbd90053dc5586cb969d225bedc093d8
Zaynab Jamal <zaynab.software2000@gmail.com>
2024-03-21 02:24:48 +0300

    AnotherChanges

--- flutter_application/lib/register/login_page.dart
+++ flutter_application/lib/register/login_page.dart
@@ -100,7 +100,7 @@ class _LoginState extends State<Login> {
                 ),
                 const Gap(50),
                 CustomTextField(
-                  label: 'Username / Email',
+                  label: 'Email',
                   controller: _emailOrUsernameController,
                   hideText: false,
                 ),
